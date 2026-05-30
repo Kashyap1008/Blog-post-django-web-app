c999d7b08bb75a54be4dd18c8a23add6c4a85e02
Kashyap1008 <kashyap11118638@gmail.com>
2026-05-30 21:01:49 +0530

    update

--- BlogPost/BlogPost/Blog/views.py
+++ BlogPost/BlogPost/Blog/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-
-
-
+posts = [
+    {
+        'author':'coreyMS',
+        'title':'blog post 1',
+        'content':'first post content  .  .  .',
+        'date_posted':'august 27 ,2018'
+    }
+    ,
+    {
+        'author':'jane doe',
+        'title':'blog post 2',
+        'content':'second post content  .  .  .',
+        'date_posted':'august 28 ,2018'
+    }
+    ]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home </h>')
+    context = { 
+        'posts' : posts
+    }
+    return render(request,'Blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h>')
+    return render(request,'Blog/about.html',{'title':'About'})
+
+
  
